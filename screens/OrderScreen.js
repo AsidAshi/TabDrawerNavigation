74592e6d3a1b38d2d85343f987e532b507570225
AsidAshi <asidashi98@gmail.com>
2021-10-16 12:15:31 +0530

    orderscreen implemented

--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -5,7 +5,10 @@ import { View, Text, StyleSheet } from 'react-native';
 const OrderScreen = ( ) => {
     return (
         <View style={styles.container}>
-            <Text>order Screen</Text>
+            <Text>order component</Text>
+            <Text>order component1</Text>
+            <Text>order component12</Text>
+            <Text>order component1223</Text>
         </View>
     )
 }
